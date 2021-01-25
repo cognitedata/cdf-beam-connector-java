9fd91f8f7f32af0928f72dd8ea625bc00e4bbdf1
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2021-01-25 16:59:36 +0100

    Add ts point json parser

--- src/main/java/com/cognite/client/servicesV1/parser/TimeseriesParser.java
+++ src/main/java/com/cognite/client/servicesV1/parser/TimeseriesParser.java
@@ -25,6 +25,7 @@ import com.cognite.v1.timeseries.proto.NumericDatapoint;
 import com.cognite.v1.timeseries.proto.StringDatapoint;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -36,6 +37,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+
+import static com.cognite.beam.io.CogniteIO.MAX_LOG_ELEMENT_LENGTH;
 
 /**
  * This class contains a set of methods to help parsing timeseries object between Cognite api representations
@@ -122,6 +125,109 @@ public class TimeseriesParser {
 
                 outputPointBuilder.setValueAggregates(aggBuilder.build());
                 outputList.add(outputPointBuilder.build());
+            }
+        }
+        return outputList;
+    }
+
+    public static List<TimeseriesPoint> parseDataPointListItem(String json) throws Exception {
+        JsonNode root = objectMapper.readTree(json);
+        List<TimeseriesPoint> outputList = new ArrayList<>();
+
+        // Hold the outer data attributes
+        long id = -1;
+        Optional<String> externalId = Optional.empty();
+        Optional<Boolean> isString = Optional.empty();
+        Optional<Boolean> isStep = Optional.empty();
+
+        // A TS point must have an id.
+        if (root.path("id").isIntegralNumber()) {
+            id = root.get("id").longValue();
+        } else {
+            throw new Exception("Unable to parse attribute: id. Item exerpt: "
+                    + json
+                    .substring(0, Math.min(json.length() - 1, MAX_LOG_ELEMENT_LENGTH)));
+        }
+
+        // Optional outer attributes
+        if (root.path("externalId").isTextual()) {
+            externalId = Optional.of(root.get("externalId").textValue());
+        }
+        if (root.path("isString").isBoolean()) {
+            isString = Optional.of(root.get("isString").booleanValue());
+        }
+        if (root.path("isStep").isBoolean()) {
+            isStep = Optional.of(root.get("isStep").booleanValue());
+        }
+
+        // Parse the inner array and produce one output per inner item.
+        if (root.path("datapoints").isArray()) {
+            ArrayNode datapoints = (ArrayNode) root.path("datapoints");
+            for (JsonNode node : datapoints) {
+                TimeseriesPoint.Builder tsPointBuilder = TimeseriesPoint.newBuilder();
+                // a datapoint must have a timestamp
+                if (node.path("timestamp").isIntegralNumber()) {
+                    tsPointBuilder.setTimestamp(node.get("timestamp").longValue());
+                } else {
+                    throw new Exception("Unable to parse attribute: datapoint.timestamp. Item exerpt: "
+                            + node.toString()
+                            .substring(0, Math.min(node.toString().length() - 1, MAX_LOG_ELEMENT_LENGTH)));
+                }
+                // all constraints are satisfied, add outer fields
+                tsPointBuilder.setId(id);
+                if (externalId.isPresent()) {
+                    tsPointBuilder.setExternalId(StringValue.of(externalId.get()));
+                }
+                if (isStep.isPresent()) {
+                    tsPointBuilder.setIsStep(BoolValue.of(isStep.get()));
+                }
+
+                // other values are optional, add inner fields.
+                // Can be one of three states: 1) numeric, 2) string and 3) aggregate
+                if (isString.isPresent() && !isString.get()) {
+                    if (node.path("value").isNumber()) {
+                        tsPointBuilder.setValueNum(node.path("value").doubleValue());
+                    }
+                } else if (isString.isPresent() && isString.get()) {
+                    if (node.path("value").isTextual()) {
+                        tsPointBuilder.setValueString(node.path("value").textValue());
+                    }
+                } else {
+                    TimeseriesPoint.Aggregates.Builder aggBuilder = TimeseriesPoint.Aggregates.newBuilder();
+                    if (node.path("average").isNumber()) {
+                        aggBuilder.setAverage(DoubleValue.of(node.path("average").doubleValue()));
+                    }
+                    if (node.path("max").isNumber()) {
+                        aggBuilder.setMax(DoubleValue.of(node.path("max").doubleValue()));
+                    }
+                    if (node.path("min").isNumber()) {
+                        aggBuilder.setMin(DoubleValue.of(node.path("min").doubleValue()));
+                    }
+                    if (node.path("count").isIntegralNumber()) {
+                        aggBuilder.setCount(Int64Value.of(node.path("count").longValue()));
+                    }
+                    if (node.path("sum").isNumber()) {
+                        aggBuilder.setSum(DoubleValue.of(node.path("sum").doubleValue()));
+                    }
+                    if (node.path("interpolation").isNumber()) {
+                        aggBuilder.setInterpolation(DoubleValue.of(node.path("interpolation").doubleValue()));
+                    }
+                    if (node.path("stepInterpolation").isNumber()) {
+                        aggBuilder.setStepInterpolation(DoubleValue.of(node.path("stepInterpolation").doubleValue()));
+                    }
+                    if (node.path("continuousVariance").isNumber()) {
+                        aggBuilder.setContinuousVariance(DoubleValue.of(node.path("continuousVariance").doubleValue()));
+                    }
+                    if (node.path("discreteVariance").isNumber()) {
+                        aggBuilder.setDiscreteVariance(DoubleValue.of(node.path("discreteVariance").doubleValue()));
+                    }
+                    if (node.path("totalVariation").isNumber()) {
+                        aggBuilder.setTotalVariation(DoubleValue.of(node.path("totalVariation").doubleValue()));
+                    }
+
+                    tsPointBuilder.setValueAggregates(aggBuilder.build());
+                }
+                outputList.add(tsPointBuilder.build());
             }
         }
         return outputList;
