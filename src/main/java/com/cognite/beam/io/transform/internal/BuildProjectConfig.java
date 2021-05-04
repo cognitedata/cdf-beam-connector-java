a20265796cdb4168dbae4bae951d8704c69f69c6
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2021-05-04 13:00:53 +0200

    Add client credentials support

--- src/main/java/com/cognite/beam/io/transform/internal/BuildProjectConfig.java
+++ src/main/java/com/cognite/beam/io/transform/internal/BuildProjectConfig.java
@@ -42,8 +42,8 @@ import java.util.List;
  * 2. File reference
  * 3. GCP secrets manager
  *
- * For all options, this transform will perform an active lookup of the project/tenant to use. I.e. the required input
- * is an api key and (optinally) a host specification.
+ * The auth config can be based on OpenID Connect client credentials or api key.
+ * OpenID Connect will take precedence over api key.
  *
  */
 @AutoValue
@@ -94,7 +94,7 @@ public abstract class BuildProjectConfig extends PTransform<PBegin, PCollection<
                                                OutputReceiver<ProjectConfig> out,
                                                ProcessContext context) throws Exception {
                         final String loggingPrefix = "BuildProjectConfig ["
-                                + RandomStringUtils.randomAlphanumeric(6)
+                                + RandomStringUtils.randomAlphanumeric(5)
                                 + "] - ";
                         LOG.debug(loggingPrefix + "Received config to process: {}", inputConfig.toString());
 
@@ -108,13 +108,26 @@ public abstract class BuildProjectConfig extends PTransform<PBegin, PCollection<
                         if (getProjectConfigParameters().isConfigured()) {
                             LOG.info(loggingPrefix + "Project config found via parameters.");
                             // if the project config is set via parameter, it should overwrite the file based config.
-                            if (null != getProjectConfigParameters().getApiKey()) {
+
+                            if (null != getProjectConfigParameters().getClientId()
+                                        && null != getProjectConfigParameters().getTokenUrl()
+                                        && null != getProjectConfigParameters().getClientSecret()) {
+                                LOG.info(loggingPrefix + "Client credentials specified via parameters");
+                                output = getProjectConfigParameters();
+                            } else if (null != getProjectConfigParameters().getClientId()
+                                    && null != getProjectConfigParameters().getTokenUrl()
+                                    && null != getProjectConfigParameters().getClientSecretGcpSecretConfig()) {
+                                LOG.info(loggingPrefix + "Client credentials specified via GCP Secret Manager");
+                                output = getProjectConfigParameters()
+                                        .withClientSecret(getGcpSecret(getProjectConfigParameters().getClientSecretGcpSecretConfig(),
+                                                loggingPrefix));
+                            } else if (null != getProjectConfigParameters().getApiKey()) {
                                 LOG.info(loggingPrefix + "Api key specified via parameters");
                                 output = getProjectConfigParameters();
                             } else if (null != getProjectConfigParameters().getApiKeyGcpSecretConfig()) {
                                 LOG.info(loggingPrefix + "Api key specified via GCP Secret Manager.");
                                 output = getProjectConfigParameters()
-                                                .withApiKey(getGcpSecret(getProjectConfigParameters().getApiKeyGcpSecretConfig(),
+                                        .withApiKey(getGcpSecret(getProjectConfigParameters().getApiKeyGcpSecretConfig(),
                                                 loggingPrefix));
                             }
                         }
